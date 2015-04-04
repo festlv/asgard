8e0ef98cf893270b2239b0ee73559f647240ba27
Reinis Veips <reinis.veips@wot.lv>
2015-04-04 12:38:42 +0300

    Specially for Zandis, added validation for uniqueness of e-mail and access card serial number

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from asgard.forms import BootstrapForm
+
 from access.forms import HexNumberInput
 
-from access.models import Zone, Tool
+from access.models import Zone, Tool, Card
+from django.contrib.auth.models import User
 
 
 class CreateUserForm(forms.Form):
@@ -17,8 +20,27 @@ class CreateUserForm(forms.Form):
     tool_access = forms.ModelMultipleChoiceField(
         Tool.objects.all(), required=False)
 
-from asgard.forms import BootstrapForm
+    def clean_email(self):
+        """
+        Validates e-mail for uniqueness, since django.contrib.auth
+        doesn't have unique constraint on e-mail.
+        """
+        v = self.cleaned_data['email'].strip()
+        if len(User.objects.filter(email__iexact=v)) > 0:
+            raise forms.ValidationError(
+                "User with this e-mail address already exists")
+        return v
 
+    def clean_card_serial_number(self):
+        """
+        Validates card serial number for uniqueness.
+        """
+        v = self.cleaned_data['card_serial_number']
+
+        if len(Card.objects.filter(serial_number=v)) > 0:
+            raise forms.ValidationError(
+                "Access card with this serial number already exists")
+        return v
 
 
 class LoginForm(BootstrapForm):
