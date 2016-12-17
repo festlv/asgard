e4981a2d8b8e997464a3d8d2bbf7c6ced9615180
Reinis Veips <reinis.veips@wot.lv>
2016-12-17 23:31:05 +0200

    Shows last access logs

--- donations/views.py
+++ donations/views.py
@@ -1,17 +1,15 @@
-import datetime
-from dateutils import relativedelta
-
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from .models import Donation
-from access.models import ZoneUsage
+from access.models import ZoneAccessLog
+
 
 @login_required
 def index(request):
 
     donations = Donation.objects.user_donations(request.user)[20:]
 
-    zone_usages = ZoneUsage.objects.filter_user(request.user)[:20]
+    zone_access = ZoneAccessLog.objects.filter_user(request.user)[:20]
 
     return render(request, 'donations/index.html', locals())
